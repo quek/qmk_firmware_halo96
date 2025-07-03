66608167bdb05010d90a8508c7d197a41f4ff260
Tahara Yoshinori <read.eval.print@gmail.com>
2025-07-03 19:16:23 +0900

    layer_state_set_usera

--- keyboards/nuphy/halo96_v2/ansi/keymaps/via/keymap.c
+++ keyboards/nuphy/halo96_v2/ansi/keymaps/via/keymap.c
@@ -1,5 +1,9 @@
 #include QMK_KEYBOARD_H
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, 2, 5, 6);
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // layer 0 Mac
